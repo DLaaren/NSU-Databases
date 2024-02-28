ee48c9682dc5333e1a4df2dda2c1dd64ea99b446
DLaaren <dlaaren@yandex.ru>
2024-02-28 15:00:16 +0700

    that will be tough

---
+++ ZooKeeper/src/main/java/nsu/fit/databases/zookeeper/ZooKeeperApplication.java
@@ -0,0 +1,11 @@
+package nsu.fit.databases.zookeeper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooKeeperApplication {
+
+    public static void main(String[] args) {
+        /// run
+    }
+}
---
+++ ZooKeeper/src/test/java/nsu/fit/databases/zookeeper/ZooKeeperApplicationTests.java
@@ -0,0 +1,14 @@
+package nsu.fit.databases.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZooKeeperApplicationTests {
+
+    @Test
+    void Test() {
+
+    }
+
+}
