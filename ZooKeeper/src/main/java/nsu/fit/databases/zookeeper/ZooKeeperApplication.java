8109c2ef5b46acfd09af114a1b0f38af747fa631
DLaaren <dlaaren@yandex.ru>
2024-03-03 19:27:22 +0700

    done Dockerfile; kinda working

--- ZooKeeper/src/main/java/nsu/fit/databases/zookeeper/ZooKeeperApplication.java
+++ ZooKeeper/src/main/java/nsu/fit/databases/zookeeper/ZooKeeperApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZooKeeperApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZooKeeperApplication.class, args);
     }
--- ZooKeeper/src/test/java/nsu/fit/databases/zookeeper/ZooKeeperApplicationTests.java
+++ ZooKeeper/src/test/java/nsu/fit/databases/zookeeper/ZooKeeperApplicationTests.java
@@ -3,12 +3,12 @@ package nsu.fit.databases.zookeeper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ZooKeeperApplicationTests {
-
-    @Test
-    void Test() {
-
-    }
-
-}
+//@SpringBootTest
+//class ZooKeeperApplicationTests {
+//
+//    @Test
+//    void Test() {
+//
+//    }
+//
+//}
